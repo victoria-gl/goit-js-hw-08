412bf839bd7ffc9cfafe786b240b77af5457f299
victoria-gl <138527509+victoria-gl@users.noreply.github.com>
2023-11-28 20:06:49 +0000

    Deploying to gh-pages from @ victoria-gl/goit-js-hw-08@df59ca1db1c1768ba3fe9091479ff9f231557ce2 🚀

---
+++ 02-video.51d3dc95.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector("#vimeo-player"),n=new Vimeo.Player(e);n.insertAdjacentHTML("beforeend",r(e)),n.addEventListener("click",handleClick);var r=function(e){};n.on("play",r)}();
+//# sourceMappingURL=02-video.51d3dc95.js.map
