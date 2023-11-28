71a6366863411787256669174a0bc42461ea9e06
victoria-gl <138527509+victoria-gl@users.noreply.github.com>
2023-11-28 21:51:31 +0100

    lesson-2-try

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,17 +1,21 @@
-
-    const iframe = document.querySelector("#vimeo-player");
-    // console.log(iframe);
-    const player = new Vimeo.Player(iframe);
-    // console.log(player);
-
-   player.insertAdjacentHTML("beforeend", onPlay(iframe));
-   player.addEventListener("click", handleClick);
-   
-
-    const onPlay = function(event) {
-            duration: 61.857
-            percent: 0.049
-            seconds: 3.034
-    };
-    
-    player.on('play', onPlay);
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const keyForStorge = "videoplayer-current-time"
+const onPlay = function (data) {
+  localStorage.setItem(keyForStorge, data.seconds);
+};
+player.on('timeupdate', throttle(onPlay, 1000));
+const currentTime = localStorage.getItem(keyForStorge);
+player
+  .setCurrentTime(currentTime)
+  .then(function (seconds) {})
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        break;
+      default:
+        break;
+    }
+  });
