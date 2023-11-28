c1392044e66549d26606ab1f6d8cd3d32dde036b
victoria-gl <138527509+victoria-gl@users.noreply.github.com>
2023-11-28 21:54:36 +0100

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,12 +1,19 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
+
+
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
+
 const keyForStorge = "videoplayer-current-time"
+
 const onPlay = function (data) {
   localStorage.setItem(keyForStorge, data.seconds);
 };
+
 player.on('timeupdate', throttle(onPlay, 1000));
+
+
 const currentTime = localStorage.getItem(keyForStorge);
 player
   .setCurrentTime(currentTime)
