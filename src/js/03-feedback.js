e99a2dff9c8521ee049e5e59258dfa5c9e92076f
victoria-gl <138527509+victoria-gl@users.noreply.github.com>
2023-11-28 22:14:23 +0100

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,30 +1,30 @@
 import throttle from "lodash.throttle";
-const form = document.querySelector('.feedback-form');
+const form = document.querySelector(".feedback-form");
 const emailInput = form.elements.email;
 const messageInput = form.elements.message;
 
 
 const formFields = () => {
-    const jsonString = localStorage.getItem('feedback-form-state');
+    const jsonString = localStorage.getItem("feedback-form-state");
     if (jsonString) {
         const formData = JSON.parse(jsonString);
         emailInput.value = formData.email;
         messageInput.value = formData.message;
     }
 };
-document.addEventListener('DOMContentLoaded', formFields);
+document.addEventListener("DOMContentLoaded", formFields);
 
 const handleSubmit = event => {
     event.preventDefault();
-    const jsnStr = localStorage.getItem('feedback-form-state');
+    const jsnStr = localStorage.getItem("feedback-form-state");
     if (jsnStr) {
         const formData = JSON.parse(jsnStr);
         console.log(formData);
-        localStorage.removeItem('feedback-form-state');
+        localStorage.removeItem("feedback-form-state");
         form.reset();
     }
 };
-form.addEventListener('submit', handleSubmit);
+form.addEventListener("submit", handleSubmit);
 
 const savedFormData = () => {
     const formData = {
@@ -32,8 +32,8 @@ const savedFormData = () => {
         message: messageInput.value,
     };
     const jsonStringThrottle = JSON.stringify(formData);
-    localStorage.setItem('feedback-form-state', jsonStringThrottle);
+    localStorage.setItem("feedback-form-state", jsonStringThrottle);
 };
 
 const throttledSaveFormData = throttle(savedFormData, 500);
-form.addEventListener('input', throttledSaveFormData);
+form.addEventListener("input", throttledSaveFormData);
