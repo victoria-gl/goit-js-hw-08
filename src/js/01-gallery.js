df59ca1db1c1768ba3fe9091479ff9f231557ce2
victoria-gl <138527509+victoria-gl@users.noreply.github.com>
2023-11-28 21:06:27 +0100

    task-1-done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,35 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+
+
+const container = document.querySelector(".gallery")
+
+container.insertAdjacentHTML("beforeend", createMarkup(galleryItems));
+
+    const lightbox = new SimpleLightbox(".gallery a", {
+        captionsData: "alt",
+        captionDelay: 250,
+});
+
+function createMarkup(arr) {
+    return arr.map(({ preview, original, description }) => `
+        <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+              <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+      data-id="${description}"
+               />
+  </a>
+</li>
+        `).join("")
+};
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
 
+    const iframe = document.querySelector("#vimeo-player");
+    // console.log(iframe);
+    const player = new Vimeo.Player(iframe);
+    // console.log(player);
+
+   player.insertAdjacentHTML("beforeend", onPlay(iframe));
+   player.addEventListener("click", handleClick);
+   
+
+    const onPlay = function(event) {
+            duration: 61.857
+            percent: 0.049
+            seconds: 3.034
+    };
+    
+    player.on('play', onPlay);
